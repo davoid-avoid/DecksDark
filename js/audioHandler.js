93dcd2ea6b25c4e334c0d5d91a6f9215b7d2d911
davoid-avoid <davetrowe@gmail.com>
2019-03-09 20:16:28 -0500

    preloader working

--- js/audioHandler.js
+++ js/audioHandler.js
@@ -1,47 +1,85 @@
 let masterVolume = 0.6;
 
-function audioUpdate(){
-setTimeout(function(){
-    SM.update()
-    audioUpdate();
-}, 60)
+function audioUpdate() {
+    setTimeout(function () {
+        SM.update()
+        audioUpdate();
+    }, 60)
 }
 
 audioUpdate();
 
 let audioPath = './assets/sound/'
 createjs.Sound.initializeDefaultPlugins();
-createjs.Sound.on("fileload", this.loadHandler, this);
+//createjs.Sound.on("fileload", this.loadHandler, this);
 
-let musicTracks2 = [
-createjs.Sound.registerSound(audioPath + "map.mp3", "map"),
-createjs.Sound.registerSound(audioPath + "easy.mp3", "easy"),
-createjs.Sound.registerSound(audioPath + "medium.mp3", "medium"),
-createjs.Sound.registerSound(audioPath + "hard.mp3", "hard"),
-createjs.Sound.registerSound(audioPath + "boss1.mp3", "boss1"),
-createjs.Sound.registerSound(audioPath + "boss2.mp3", "boss2"),
-createjs.Sound.registerSound(audioPath + "endbattle.mp3", "endbattle"),
-createjs.Sound.registerSound(audioPath + "returnbonfire.mp3", "returnbonfire"),
+var queue = "";
 
-createjs.Sound.registerSound(audioPath + "hit.wav", "hit"),
-createjs.Sound.registerSound(audioPath + "kill.wav", "kill"),
-createjs.Sound.registerSound(audioPath + "shield.wav", "shield"),
-createjs.Sound.registerSound(audioPath + "heal.wav", "heal"),
-createjs.Sound.registerSound(audioPath + "special.wav", "special"),
-createjs.Sound.registerSound(audioPath + "endturn.wav", "endturn"),
-createjs.Sound.registerSound(audioPath + "discard.wav", "discard"),
-createjs.Sound.registerSound(audioPath + "nodamage.wav", "nodamage"),
-createjs.Sound.registerSound(audioPath + "death.wav", "death"),
-createjs.Sound.registerSound(audioPath + "levelup.wav", "levelup"),
-createjs.Sound.registerSound(audioPath + "bonfire.wav", "bonfire"),
-createjs.Sound.registerSound(audioPath + "readyweapon.wav", "enableweapon"),
-createjs.Sound.registerSound(audioPath + "disableweapon.wav", "disableweapon"),
-]
+document.addEventListener('DOMContentLoaded', function () {
+    var queue = new createjs.LoadQueue();
+    queue.installPlugin(createjs.Sound);
+    queue.on("fileload", handleFileLoad);
+    queue.on('progress', handleProgress);
+    queue.on("complete", handleComplete);
 
-function loadHandler(event) {
-    // This is fired for each sound that is registered.
-    console.log('completed loading ' + event)  // play using id.  Could also use full sourcepath or event.src.
+
+    queue.loadManifest([
+        { id: "map", src: audioPath + "map.mp3" },
+        { id: "easy", src: audioPath + "easy.mp3" },
+        { id: "medium", src: audioPath + "medium.mp3" },
+        { id: "hard", src: audioPath + "hard.mp3" },
+        { id: "boss1", src: audioPath + "boss1.mp3" },
+        { id: "boss2", src: audioPath + "boss2.mp3" },
+        { id: "endbattle", src: audioPath + "endbattle.mp3" },
+        { id: "returnbonfire", src: audioPath + "returnbonfire.mp3" },
+
+        { id: "hit", src: audioPath + "hit.wav" },
+        { id: "kill", src: audioPath + "kill.wav" },
+        { id: "shield", src: audioPath + "shield.wav" },
+        { id: "heal", src: audioPath + "heal.wav" },
+        { id: "special", src: audioPath + "special.wav" },
+        { id: "endturn", src: audioPath + "endturn.wav" },
+        { id: "discard", src: audioPath + "discard.wav" },
+        { id: "nodamage", src: audioPath + "nodamage.wav" },
+        { id: "death", src: audioPath + "death.wav" },
+        { id: "leveup", src: audioPath + "levelup.wav" },
+        { id: "bonfire", src: audioPath + "bonfire.wav" },
+        { id: "enableweapon", src: audioPath + "readyweapon.wav" },
+        { id: "disableweapon", src: audioPath + "disableweapon.wav" },
+
+        { id: "character-sheet", src: "./assets/enemies/character-strip.png" },
+        { id: "bonfire-sheet", src: "./assets/bonfire/bonfire-sheet.png" },
+        { id: "icon-sheet", src: "./assets/icons/icon-sheet.png" },
+        { id: "map-sprite-sheet", src: "./assets/maps/map-sprite-sheet.png" },
+        { id: "map1", src: "./assets/maps/map1.png" },
+        { id: "map2", src: "./assets/maps/map2.png" },
+        { id: "completed", src: "./assets/maps/completed.png" },
+        { id: "title-screen", src: "./assets/icons/title.png" },
+    ]);
+
+});
+
+function handleFileLoad(event) {
+    console.log('loaded ' + event.item.id + " " + event.item.src + " " + event.item.type)
 }
+
+function handleProgress(event) {
+    let progress = Math.round(event.loaded * 100);
+    console.log(progress);
+    let loadingBar = document.getElementById('load-bar-inner');
+    loadingBar.style.width = progress + "%";
+}
+
+function handleComplete(event) {
+    console.log('all loaded')
+
+    let loading = document.getElementById('loading');
+    let title = document.getElementById('title');
+
+    title.classList.remove('hidden');
+    loading.classList.add('hidden')
+}
+
 
 
 let musicList = ["map", "easy", "medium", "hard", "boss1", "boss2", "endbattle", "returnbonfire"]
@@ -49,7 +87,7 @@ let musicList = ["map", "easy", "medium", "hard", "boss1", "boss2", "endbattle",
 function fadeOutAll(key) {
     musicList.forEach(song => {
         if (song !== key) {
-            SM.stopMusic(song, 500)
+            SM.stopMusic(song, 300)
         }
     })
 
@@ -57,38 +95,38 @@ function fadeOutAll(key) {
 
 function playMusic(key) {
     fadeOutAll(key);
-    setTimeout(function(){
+    setTimeout(function () {
         var currentPlaying = SM.playMusic(key, -1, 1)
     }, 1000)
 }
 
-function playSFX(key){
+function playSFX(key) {
     SM.playSound(key)
 }
 
-function soundControl(){
+function soundControl() {
 
     let volume = createjs.Sound.muted;
-    if (volume === false){
+    if (volume === false) {
         createjs.Sound.muted = true;
     }
-    if (volume === true){
+    if (volume === true) {
         createjs.Sound.muted = false;
     }
     updateReadout();
 }
 
-var SM = (function(){
+var SM = (function () {
     var musics = {};
     /**
      * CreateJS Sound Manager
      * @returns {null}
      */
-    function SM(){
+    function SM() {
         throw new Error("This class can't be instantiated");
         return null;
     }
-    
+
     /**
      * Play music. This is only for playing musics. Use SM.playSound to play sound effects
      * @static
@@ -97,45 +135,45 @@ var SM = (function(){
      * @param {Number} fadeIn Number of milliseconds to fade in
      * @returns {void}
      */
-    SM.playMusic = function(id,repeat,fadeIn){
-        if(musics[id] && musics[id].playing){
+    SM.playMusic = function (id, repeat, fadeIn) {
+        if (musics[id] && musics[id].playing) {
             return;
         }
-        repeat = repeat||0;
-        fadeIn = (!fadeIn)?0:fadeIn;
+        repeat = repeat || 0;
+        fadeIn = (!fadeIn) ? 0 : fadeIn;
         var instance = createjs.Sound.play(id);
-        instance.volume = (fadeIn!==0)?0:masterVolume;
+        instance.volume = (fadeIn !== 0) ? 0 : masterVolume;
         var o = {
-            instance    : instance,
-            playing     : true,
-            repeat      : (repeat>=0)?repeat:0,
-            loop        : (repeat===-1)?true:false,
-            fadeStep    : 1000/(60*fadeIn),
-            fadeType    : "FADE_IN"
+            instance: instance,
+            playing: true,
+            repeat: (repeat >= 0) ? repeat : 0,
+            loop: (repeat === -1) ? true : false,
+            fadeStep: 1000 / (60 * fadeIn),
+            fadeType: "FADE_IN"
         };
         musics[id] = o;
-        instance.addEventListener("complete",function(){
+        instance.addEventListener("complete", function () {
             SM.musicComplete(o);
         });
     };
-    SM.update = function(){
-        for(var id in musics){
+    SM.update = function () {
+        for (var id in musics) {
             var o = musics[id];
-            if(o.playing){
-                if(o.fadeType === "FADE_IN"){
-                    if (o.fadeStep === "NaN"){
+            if (o.playing) {
+                if (o.fadeType === "FADE_IN") {
+                    if (o.fadeStep === "NaN") {
                         o.fadeStep = 0;
                     }
                     o.instance.volume += o.fadeStep;
-                    if(o.instance.volume >= masterVolume){
+                    if (o.instance.volume >= masterVolume) {
                         o.instance.volume = masterVolume;
                     }
-                }else{
-                    if (o.fadeStep === "NaN"){
+                } else {
+                    if (o.fadeStep === "NaN") {
                         o.fadeStep = 0;
                     }
                     o.instance.volume -= o.fadeStep;
-                    if(o.instance.volume <= 0){
+                    if (o.instance.volume <= 0) {
                         o.instance.volume = 0;
                         SM.stopMusic(id);
                     }
@@ -143,13 +181,13 @@ var SM = (function(){
             }
         }
     };
-   SM.musicComplete = function(o){
+    SM.musicComplete = function (o) {
         o.playing = false;
         o.repeat -= 1;
-        if(o.loop===true){
+        if (o.loop === true) {
             o.instance.play();
             o.playing = true;
-        }else if(o.repeat>0){
+        } else if (o.repeat > 0) {
             o.instance.play();
             o.playing = true;
         }
@@ -161,15 +199,15 @@ var SM = (function(){
      * @param {Number}  fadeOut Number of milliseconds to fadeOut
      * @returns {void}
      */
-    SM.stopMusic = function(id,fadeOut){
+    SM.stopMusic = function (id, fadeOut) {
         var o = musics[id];
-        if (o !== undefined){
-            fadeOut = (!fadeOut)?0:fadeOut;
-            if(o && o.playing){
+        if (o !== undefined) {
+            fadeOut = (!fadeOut) ? 0 : fadeOut;
+            if (o && o.playing) {
                 o.fadeType = "FADE_OUT";
-                o.fadeStep = ((o.instance.volume*1000)/(60*fadeOut)).toFixed(2);
+                o.fadeStep = ((o.instance.volume * 1000) / (60 * fadeOut)).toFixed(2);
 
-                if (o.instance.volume === 0){
+                if (o.instance.volume === 0) {
                     o.playing = false;
                 }
             }
@@ -180,7 +218,7 @@ var SM = (function(){
      * @param {String} id
      * @returns {void}
      */
-    SM.playSound = function(id){
+    SM.playSound = function (id) {
         createjs.Sound.play(id);
     };
     /**
@@ -188,17 +226,17 @@ var SM = (function(){
      * @param {String} id
      * @returns {void}
      */
-    SM.stopSound = function(id){
+    SM.stopSound = function (id) {
         createjs.Sound.stop(id);
     };
- /**
-     * Stop playing all sounds
-     * @param {type} fadeOut
-     * @returns {void}
-     */
-    SM.stopAllMusics = function(fadeOut){
-        for(var id in musics){
-            SM.stopMusic(id,fadeOut);
+    /**
+        * Stop playing all sounds
+        * @param {type} fadeOut
+        * @returns {void}
+        */
+    SM.stopAllMusics = function (fadeOut) {
+        for (var id in musics) {
+            SM.stopMusic(id, fadeOut);
         }
     };
     return SM;
